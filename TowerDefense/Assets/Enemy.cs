3a816e713cfa7a898e4245f8957ec69c7e426cdd
Bilal-Yucell <bilallyucell@gmail.com>
2023-06-09 03:57:47 +0300

    Düşmanın Kuleye Gidişi Ayarlandı
    Artık kontrol noktaları sayesinde düşman belirli bir yolda gidiyor ve düşman bir sonraki hedef noktasına dönecek şekilde rotasyon ayarı yapıldı.

---
+++ TowerDefense/Assets/Enemy.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 100f;
+
+    private Transform target;
+    private int waveintIndex = 0;
+    public float rotationSpeedMultiplier = 5f;
+
+
+    void Start()
+    {
+        target = Waypoints.points[0];
+    }
+
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+        Quaternion rotation = Quaternion.LookRotation(dir);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * rotationSpeedMultiplier);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.4f)
+        {
+            GetNextWaypoint();
+        }
+    }
+
+    void GetNextWaypoint()
+    {
+        if (waveintIndex >= Waypoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        waveintIndex++;
+        target = Waypoints.points[waveintIndex];
+        // transform.rotation *= Quaternion.Euler(0f, rotationAngle, 0f);
+    }
+
+}
