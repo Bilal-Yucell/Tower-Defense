b03eb90b9ae204550a4c4af81ee6b89e51773f5d
Bilal-Yucell <bilallyucell@gmail.com>
2023-06-13 23:14:10 +0300

    Düşman Yolu Üzerine Konulacak Kule Eklendi
    Düşmanın kulemize gelmesini engelleyecek, düşman yolu üzerine kurulacak küçük kule üstünde asker ile oyuna eklendi.

---
+++ TowerDefense/Assets/Scripts/Turret.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+
+    public Transform target;
+    public float range = 15f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+
+}
